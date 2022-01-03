50eed74096c4a09cbdaaf0bf68b9339092abdb0f
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-03 11:13:31 +0500

    js

--- script.js
+++ script.js
@@ -1314,4 +1314,31 @@ function character() {
       }
       Evenodd(9)
 
+
+
+
+       // program 6
+    // Write a js program to check whether a number is positive, negative or zero using switch case.
+    function number1(x) {
       
+        switch (x > 0) {
+          case true:
+            console.log(x," Is Positive Number");
+            break;
+          case false:
+            switch (x < 0) {
+              case true:
+                console.log(x," is Negative");
+                break;
+            
+              default:
+                console.log(x," is Zero");
+                break;
+            }
+            break;
+            default:
+              console.log("yess");
+              break;
+          }
+      }
+      number1(0)
